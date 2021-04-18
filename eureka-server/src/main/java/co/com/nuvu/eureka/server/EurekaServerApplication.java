27094bd1b88212e7d1749a87148860093eb228b6
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 14:35:21 -0500

    Creación del proyecto eureka-server
    Creación del proyecto para el servidor de nombre

---
+++ eureka-server/src/main/java/co/com/nuvu/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package co.com.nuvu.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}

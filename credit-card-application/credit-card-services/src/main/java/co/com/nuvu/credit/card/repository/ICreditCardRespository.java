b186b939d7b152f64cea139960568c6ec339485e
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 01:44:59 -0500

    Modificar repositorio

--- credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/repository/ICreditCardRespository.java
+++ credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/repository/ICreditCardRespository.java
@@ -1,9 +1,10 @@
 package co.com.nuvu.credit.card.repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import co.com.nuvu.credit.card.model.CreditCard;
 
-public interface ICreditCardRespository extends CrudRepository<CreditCard, String> {
+public interface ICreditCardRespository extends MongoRepository<CreditCard, String> {
 
 }

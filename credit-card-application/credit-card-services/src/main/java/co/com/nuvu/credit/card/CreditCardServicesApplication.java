ebe3ac629ac9a9a2ea1ce0135a3e6bc0e3ea4157
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 00:01:20 -0500

    Creación proyecto credit-card-application
    Creación de la aplicación para el manejo de las tarjeta de crédito de los clientes

---
+++ credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/CreditCardServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.nuvu.credit.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardServicesApplication.class, args);
+	}
+
+}
---
+++ credit-card-application/credit-card-services/src/test/java/co/com/nuvu/credit/card/CreditCardServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.nuvu.credit.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c777efaf0cc38d2f9b3d77c3b01861ebadea4f60
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 17:50:55 -0500

    Modificar el tipo de objeto del repositorio
    Modificar el tipo de objeto que se guarda en la base de datos

--- person-application/person-services/src/main/java/co/com/nuvu/person/services/repository/IPersonRepository.java
+++ person-application/person-services/src/main/java/co/com/nuvu/person/services/repository/IPersonRepository.java
@@ -2,8 +2,8 @@ package co.com.nuvu.person.services.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import co.com.nuvu.person.commons.dto.PersonDTO;
+import co.com.nuvu.person.services.model.Person;
 
-public interface IPersonRepository extends MongoRepository<PersonDTO, String> {
+public interface IPersonRepository extends MongoRepository<Person, String> {
 
 }

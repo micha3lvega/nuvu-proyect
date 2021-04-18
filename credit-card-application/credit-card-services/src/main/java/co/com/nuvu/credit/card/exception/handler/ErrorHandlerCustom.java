704cbd81973d39888017b5b6c555876232352541
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 02:53:41 -0500

    Creación de handler para las excepciones
    Creación de los handlers que escuchan las excepciones de los servicios.

---
+++ credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/exception/handler/ErrorHandlerCustom.java
@@ -0,0 +1,37 @@
+package co.com.nuvu.credit.card.exception.handler;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import co.com.nuvu.credit.card.exception.ExceptionErrorInfo;
+
+@ControllerAdvice
+public class ErrorHandlerCustom {
+
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ExceptionErrorInfo> methodArgumentNotValidException(HttpServletRequest request,
+			RuntimeException e) {
+
+		ResponseStatus responseStatus = null;
+		for (int i = 0; i < e.getClass().getAnnotations().length; i++) {
+
+			Object object = e.getClass().getAnnotations()[i];
+			if (object instanceof ResponseStatus) {
+				responseStatus = (ResponseStatus) object;
+
+				ExceptionErrorInfo errorInfo = new ExceptionErrorInfo(responseStatus.code().value(),
+						responseStatus.reason(), request.getRequestURI());
+				// return error info object with standard json
+				return new ResponseEntity<>(errorInfo, responseStatus.value());
+			}
+
+		}
+
+		return null;
+	}
+
+}

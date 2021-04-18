a0f20ac281fb3dfc7c17d5cb4fdcff58d9ac8817
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 17:12:39 -0500

    Activar auditoria de mongo

--- credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/CreditCardServicesApplication.java
+++ credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/CreditCardServicesApplication.java
@@ -2,7 +2,11 @@ package co.com.nuvu.credit.card;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableEurekaClient
+@EnableMongoAuditing
 @SpringBootApplication
 public class CreditCardServicesApplication {
 

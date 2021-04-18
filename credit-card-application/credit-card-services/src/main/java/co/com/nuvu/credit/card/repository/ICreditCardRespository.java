f0fd57680aab7e0b8f4dbd31066aa7ff8d52cc29
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 02:55:36 -0500

    Eliminar import

--- credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/repository/ICreditCardRespository.java
+++ credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/repository/ICreditCardRespository.java
@@ -1,7 +1,6 @@
 package co.com.nuvu.credit.card.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import co.com.nuvu.credit.card.model.CreditCard;
 

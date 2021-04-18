c8941d61e1538c0071f84e86db5180ecc619f8b8
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 18:53:06 -0500

    Agregar equals a la entidad

--- credit-card-application/credit-card-commons/src/main/java/co/com/nuvu/credit/card/dto/CreditCardDTO.java
+++ credit-card-application/credit-card-commons/src/main/java/co/com/nuvu/credit/card/dto/CreditCardDTO.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package co.com.nuvu.credit.card.dto;
 
@@ -16,9 +16,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * 
+ *
  * Clase que representa el DTO de una tarjeta de credito
- * 
+ *
  * @author Michael Vega
  *
  */
@@ -46,5 +46,79 @@ public class CreditCardDTO implements Serializable {
 	private Date createdAt;
 
 	private Date updatedAt;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		CreditCardDTO other = (CreditCardDTO) obj;
+		if (cardType != other.cardType) {
+			return false;
+		}
+		if (createdAt == null) {
+			if (other.createdAt != null) {
+				return false;
+			}
+		} else if (!createdAt.equals(other.createdAt)) {
+			return false;
+		}
+		if (cvv == null) {
+			if (other.cvv != null) {
+				return false;
+			}
+		} else if (!cvv.equals(other.cvv)) {
+			return false;
+		}
+		if (expirationDate == null) {
+			if (other.expirationDate != null) {
+				return false;
+			}
+		} else if (!expirationDate.equals(other.expirationDate)) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (number == null) {
+			if (other.number != null) {
+				return false;
+			}
+		} else if (!number.equals(other.number)) {
+			return false;
+		}
+		if (updatedAt == null) {
+			if (other.updatedAt != null) {
+				return false;
+			}
+		} else if (!updatedAt.equals(other.updatedAt)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result) + ((cardType == null) ? 0 : cardType.hashCode());
+		result = (prime * result) + ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = (prime * result) + ((cvv == null) ? 0 : cvv.hashCode());
+		result = (prime * result) + ((expirationDate == null) ? 0 : expirationDate.hashCode());
+		result = (prime * result) + ((id == null) ? 0 : id.hashCode());
+		result = (prime * result) + ((number == null) ? 0 : number.hashCode());
+		result = (prime * result) + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+		return result;
+	}
 
 }

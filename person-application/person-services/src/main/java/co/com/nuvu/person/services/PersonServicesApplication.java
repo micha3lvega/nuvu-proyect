9eebcfde7bc48c8e212752678f74ef1ed87764f7
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 18:52:41 -0500

    Activar auditoria de la entidad

--- person-application/person-services/src/main/java/co/com/nuvu/person/services/PersonServicesApplication.java
+++ person-application/person-services/src/main/java/co/com/nuvu/person/services/PersonServicesApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableEurekaClient
 @EnableFeignClients
+@EnableMongoAuditing
 @SpringBootApplication
 public class PersonServicesApplication {
 

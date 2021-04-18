a545bc83091894b2cd672e9c0986e1be02420cc4
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 17:48:08 -0500

    Activar clientes de openfeing

--- credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/CreditCardServicesApplication.java
+++ credit-card-application/credit-card-services/src/main/java/co/com/nuvu/credit/card/CreditCardServicesApplication.java
@@ -3,9 +3,11 @@ package co.com.nuvu.credit.card;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableEurekaClient
+@EnableFeignClients
 @EnableMongoAuditing
 @SpringBootApplication
 public class CreditCardServicesApplication {

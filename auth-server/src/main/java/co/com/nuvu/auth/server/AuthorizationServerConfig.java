d45bd2bf162f18fa56d979a804242873b14ae0f8
Michael Vega <ECM3572R@claro.com.co>
2021-04-19 00:31:21 -0500

    Configuración de las rutas para usar JWT

--- auth-server/src/main/java/co/com/nuvu/auth/server/AuthorizationServerConfig.java
+++ auth-server/src/main/java/co/com/nuvu/auth/server/AuthorizationServerConfig.java
@@ -1,13 +1,17 @@
 package co.com.nuvu.auth.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -29,5 +33,24 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		super.configure(clients);
 	}
 
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+		endpoints.authenticationManager(authenticationManager).accessTokenConverter(accessTokenConverter())
+		.tokenStore(tokenStore());
+
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("co.com.nuvu");
+		return jwtAccessTokenConverter;
+	}
 
 }

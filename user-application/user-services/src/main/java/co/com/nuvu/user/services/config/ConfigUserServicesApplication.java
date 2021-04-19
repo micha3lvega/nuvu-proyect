53451e8d8ed2a899a83ab59d575104a052c2d299
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 23:16:29 -0500

    Crear clase de configuración

---
+++ user-application/user-services/src/main/java/co/com/nuvu/user/services/config/ConfigUserServicesApplication.java
@@ -0,0 +1,23 @@
+package co.com.nuvu.user.services.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class ConfigUserServicesApplication {
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+
+}

67aa1e29fd0c787fac54213da88e11750d1311b6
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 19:53:22 -0500

    Sobrescribir los métodos equals y hashCode

--- person-application/person-commons/src/main/java/co/com/nuvu/person/commons/dto/PersonDTO.java
+++ person-application/person-commons/src/main/java/co/com/nuvu/person/commons/dto/PersonDTO.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package co.com.nuvu.person.commons.dto;
 
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 
 /**
  * Clase que representa el DTO de una persona
- * 
+ *
  * @author Michael Vega
  *
  */
@@ -53,5 +53,99 @@ public class PersonDTO implements Serializable {
 	private Date createdAt;
 
 	private Date updatedAt;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		PersonDTO other = (PersonDTO) obj;
+		if (createdAt == null) {
+			if (other.createdAt != null) {
+				return false;
+			}
+		} else if (!createdAt.equals(other.createdAt)) {
+			return false;
+		}
+		if (creditCard == null) {
+			if (other.creditCard != null) {
+				return false;
+			}
+		} else if (!creditCard.equals(other.creditCard)) {
+			return false;
+		}
+		if (document == null) {
+			if (other.document != null) {
+				return false;
+			}
+		} else if (!document.equals(other.document)) {
+			return false;
+		}
+		if (email == null) {
+			if (other.email != null) {
+				return false;
+			}
+		} else if (!email.equals(other.email)) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (lastName == null) {
+			if (other.lastName != null) {
+				return false;
+			}
+		} else if (!lastName.equals(other.lastName)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (phone == null) {
+			if (other.phone != null) {
+				return false;
+			}
+		} else if (!phone.equals(other.phone)) {
+			return false;
+		}
+		if (updatedAt == null) {
+			if (other.updatedAt != null) {
+				return false;
+			}
+		} else if (!updatedAt.equals(other.updatedAt)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result) + ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = (prime * result) + ((creditCard == null) ? 0 : creditCard.hashCode());
+		result = (prime * result) + ((document == null) ? 0 : document.hashCode());
+		result = (prime * result) + ((email == null) ? 0 : email.hashCode());
+		result = (prime * result) + ((id == null) ? 0 : id.hashCode());
+		result = (prime * result) + ((lastName == null) ? 0 : lastName.hashCode());
+		result = (prime * result) + ((name == null) ? 0 : name.hashCode());
+		result = (prime * result) + ((phone == null) ? 0 : phone.hashCode());
+		result = (prime * result) + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+		return result;
+	}
 
 }

a595d8a23b500c71cd73760eb3f0eba7da79a42c
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 23:57:41 -0500

    Crear proyecto para el servidor de autenticación

---
+++ auth-server/src/test/java/co/com/nuvu/auth/server/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.nuvu.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

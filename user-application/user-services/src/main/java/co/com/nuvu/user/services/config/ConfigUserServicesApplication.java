819647b8265d6f22e89aed41842e77018ffa0b6d
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 23:31:02 -0500

    Cambiar el tipo del encoder

--- user-application/user-services/src/main/java/co/com/nuvu/user/services/config/ConfigUserServicesApplication.java
+++ user-application/user-services/src/main/java/co/com/nuvu/user/services/config/ConfigUserServicesApplication.java
@@ -4,13 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ConfigUserServicesApplication {
 
 	@Bean
-	public PasswordEncoder encoder() {
+	public BCryptPasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
 	}
 

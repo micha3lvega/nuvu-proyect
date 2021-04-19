3cd76429d80e93e1b8f9034b3d3029a01acdfb4c
Michael Vega <ECM3572R@claro.com.co>
2021-04-18 21:42:19 -0500

    Activar cliente eureka

--- zuul-server/src/main/java/co/com/nuvu/zuul/server/ZuulServerApplication.java
+++ zuul-server/src/main/java/co/com/nuvu/zuul/server/ZuulServerApplication.java
@@ -2,9 +2,11 @@ package co.com.nuvu.zuul.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ZuulServerApplication {
 
